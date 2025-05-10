71175fe0dd13cbabdaa77cc46170108058a77b5d
twinsynth <90240470+twinsynth@users.noreply.github.com>
2025-05-10 11:03:25 +1000

    Update topics.js

--- assets/js/topics.js
+++ assets/js/topics.js
@@ -1,1 +1,20 @@
+// This file could be used to dynamically generate topics checkboxes in topics.html
+
+const topics = [
+  { id: "mindbenders", name: "Mind Benders" },
+  { id: "poptalk", name: "Pop Talk" },
+  { id: "wanderlust", name: "Wanderlust" },
+  { id: "hottakes", name: "Hot Takes" },
+  { id: "forthelols", name: "For the LOLs" }
+];
+
+const form = document.getElementById("topics-form");
+if (form) {
+  topics.forEach(topic => {
+    const label = document.createElement("label");
+    label.innerHTML = `<input type="checkbox" name="topic" value="${topic.id}" checked /> ${topic.name}`;
+    form.appendChild(label);
+    form.appendChild(document.createElement("br"));
+  });
+}
 
