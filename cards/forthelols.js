e56c246174f4a79001f09fff49436ed89809cc86
twinsynth <90240470+twinsynth@users.noreply.github.com>
2025-05-15 21:23:35 +1000

    Update forthelols.js

--- cards/forthelols.js
+++ cards/forthelols.js
@@ -5,12 +5,20 @@ export default {
     "Pineapple belongs on pizza.",
     "If animals could talk, cats would be the rudest.",
     "Your most embarrassing moment could be turned into a hilarious movie scene.",
-    "Toes for fingers would be more useful than fingers for toes.",
-    "Aliens would probably find our obsession with reality TV the most confusing.",
-    "You’d survive a zombie apocalypse purely out of luck.",
-    "A penguin in a top hat would be the ultimate party guest.",
-    "If you trip in public, pretending to break into a dance move saves face.",
+    "Heat Vision or Icy Breath?",
+    "Dad Jokes are actaully good.",
+    "If you were on a zombie tv show, how many seasons are you surviving?",
+    "You farm for 😂 on groupchats / teams ",
+    "Travel to the future or to the past?",
     "You’ve practiced your Oscar acceptance speech at least once.",
     "A good meme is better than a motivational quote any day."
+    "Life is boring without friends playing pranks on each other.",
+    "What's the last reel you liked?",
+    "What's on your discovery page?",
+    "I know what ItalianBrainRot is.",
+    "This game was made with ChatGPT.",
+    "All these cards were written by ChatGPT.",
+    "Go get a drink.",
+    
   ]
 };
