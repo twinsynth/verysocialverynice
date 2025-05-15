2c00beeabc1be72e4271ab9043caf89633723bd0
twinsynth <90240470+twinsynth@users.noreply.github.com>
2025-05-15 21:53:50 +1000

    Update hottakes.js

--- cards/hottakes.js
+++ cards/hottakes.js
@@ -3,7 +3,7 @@ export default {
   color: "#c0392b",
   cards: [
     "Cancel culture is an effective way to hold people accountable.",
-    "Billionaires have a moral responsibility to redistribute their wealth.",
+    "Billionaires are unhinged.",
     "Mandatory voting would improve democracy.",
     "People should be allowed to say anything online.",
     "Technology is making us more disconnected despite being 'connected'.",
@@ -12,5 +12,19 @@ export default {
     "Eating meat in the modern world is unethical.",
     "Universities are overrated and overpriced.",
     "People care more about being right than being kind."
+    "Android is better than Apple.",
+    "Aliens are real.",
+    "Four day work weeks should be a thing.",
+    "Onlyfans",
+    "Political debates are fun.",
+    "There will always be war.",
+    "AI good. very nice.",
+    "It's good to be patriotic.",
+    "We don't live in a simulation.",
+    "Space exploration is good for humanity.",
+    "Podcasts are overrated.",
+    "It's okay to pirate a movie.",
+    "Youtube Music is better than Spotify.",
+    
   ]
 };
